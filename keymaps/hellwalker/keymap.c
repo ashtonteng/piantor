484fad29f863cc3283f481d4cbaca8813cfb7112
Ashton Teng <ateng@foresitelabs.com>
2023-04-26 15:50:44 -0700

    add combos

--- keymaps/hellwalker/keymap.c
+++ keymaps/hellwalker/keymap.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
       * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ - │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
+      * │Esc│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
+      * │Ctl│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │Sft├───┐           ┌───┤Ent│
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [0] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_LSFT, KC_BSPC, KC_LGUI,           MO(1),  KC_SPC,  KC_ENT
     ),
      /*
@@ -58,10 +58,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
+    [2] = LAYOUT_split_3x6_3(
         KC_NO,   KC_NO,    KC_7,    KC_8,    KC_9,    S(KC_EQL),                            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,   KC_NO,    KC_4,    KC_5,    KC_6,    KC_MINS,                              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,   KC_NO,    KC_1,    KC_2,    KC_3,    KC_DOT,                               KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
                                              KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     )
+};
+
+// COMBOS
+
+const uint16_t PROGMEM mo_layer2_combo[] = {KC_SPC, KC_ENT, COMBO_END};
+const uint16_t PROGMEM s_kc1_combo[] = {KC_Q, KC_A, COMBO_END};
+const uint16_t PROGMEM s_kc2_combo[] = {KC_W, KC_S, COMBO_END};
+const uint16_t PROGMEM s_kc3_combo[] = {KC_E, KC_D, COMBO_END};
+const uint16_t PROGMEM s_kc4_combo[] = {KC_R, KC_F, COMBO_END};
+const uint16_t PROGMEM s_kc5_combo[] = {KC_T, KC_G, COMBO_END};
+const uint16_t PROGMEM s_kc6_combo[] = {KC_Y, KC_H, COMBO_END};
+const uint16_t PROGMEM s_kc7_combo[] = {KC_U, KC_J, COMBO_END};
+const uint16_t PROGMEM s_kc8_combo[] = {KC_I, KC_K, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(mo_layer2_combo, MO(2)),
+    COMBO(s_kc1_combo, S(KC_1)),
+    COMBO(s_kc2_combo, S(KC_2)),
+    COMBO(s_kc3_combo, S(KC_3)),
+    COMBO(s_kc4_combo, S(KC_4)),
+    COMBO(s_kc5_combo, S(KC_5)),
+    COMBO(s_kc6_combo, S(KC_6)),
+    COMBO(s_kc7_combo, S(KC_7)),
+    COMBO(s_kc8_combo, S(KC_8)),
 };
