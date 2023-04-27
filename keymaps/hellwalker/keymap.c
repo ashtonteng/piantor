e73d7bf78cdc59fb3f3c4539ac5b8361e68dc70e
Ashton Teng <ateng@foresitelabs.com>
2023-04-26 17:18:41 -0700

    forgot 0

--- keymaps/hellwalker/keymap.c
+++ keymaps/hellwalker/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │   │   │ 1 │ 2 │ 3 │ . │       │   │   │   │   │   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
-      *               │   ├───┐           ┌───┤   │
+      *               │ 0 ├───┐           ┌───┤   │
       *               └───┤   ├───┐   ┌───┤   ├───┘
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,    KC_7,    KC_8,    KC_9,    S(KC_EQL),                            KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,   KC_NO,    KC_4,    KC_5,    KC_6,    KC_MINS,                              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,   KC_NO,    KC_1,    KC_2,    KC_3,    KC_DOT,                               KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+                                             KC_0, KC_TRNS, KC_TRNS,              KC_TRNS,  KC_TRNS,  KC_TRNS
     )
 };
 
